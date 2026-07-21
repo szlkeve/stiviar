b90ea46dcc683ed2e2f78b3f5e9ff237f67d9521
Keve <sz.l.keve@gmail.com>
2026-07-21 18:41:41 +0200

    add cors

--- server/rest-backend/src/index.ts
+++ server/rest-backend/src/index.ts
@@ -1,7 +1,11 @@
+import cors from 'cors';
 import express from 'express';
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
+
 
 app.get('/api/counter', (_req, res) => {
     res.json({count: 42});
