99457b6c9fccdac00f170a201589370e6026bb68
Keve <sz.l.keve@gmail.com>
2026-07-29 22:01:34 +0200

    remove config

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -1,5 +1,5 @@
-import {themes as prismThemes} from "prism-react-renderer";
-import type {Config} from "@docusaurus/types";
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
 import type * as Preset from "@docusaurus/preset-classic";
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
@@ -37,7 +37,6 @@ const config: Config = {
 
   presets: [
     [
-      "@docusaurus/preset-classic",
       "classic",
       {
         docs: {
@@ -66,12 +65,6 @@ const config: Config = {
           customCss: "./src/css/custom.css",
         },
       } satisfies Preset.Options,
-    ],
-    [
-      "docusaurus-preset-shiki-twoslash",
-      {
-        themes: ["min-light", "nord"],
-      },
     ],
   ],
 
