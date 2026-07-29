1ec329e47c7a587448ce17ab2c891a1830d48a6e
Keve <sz.l.keve@gmail.com>
2026-07-29 20:45:42 +0200

    refactor

---
+++ packages/react/src/createUseFetch.ts
@@ -0,0 +1,29 @@
+import { ApiModel, BaseApiTypeMap, Options, UseFetchArgs } from "./lib/types";
+import { validate } from "./lib/validate";
+import { QueryClient, useQuery } from "@tanstack/react-query";
+import { defaultFetchFn } from "./lib/defaultFetchFn";
+
+export function createUseFetch<ApiTypeMap extends BaseApiTypeMap>(
+  apiModel: ApiModel<ApiTypeMap>,
+  client: QueryClient,
+  options?: Options,
+) {
+  const fetchFn = options?.fetchFn ?? defaultFetchFn;
+  function useFetch<NAME extends keyof ApiTypeMap>(
+    ...args: UseFetchArgs<ApiTypeMap, NAME>
+  ) {
+    const [stateName, params] = args;
+    type StateType = ApiTypeMap[NAME]["type"];
+    const { url, schema } = apiModel[stateName];
+    const resolvedUrl: string = typeof url === "string" ? url : url(params);
+    const paramKeys = Object.values(params ?? {});
+    const queryKey = [stateName, ...paramKeys];
+    const queryFn = async (): Promise<StateType> =>
+      validate(schema, await fetchFn(resolvedUrl));
+    const { data, isLoading, error } = useQuery({ queryKey, queryFn }, client);
+    const dataWithType: StateType | undefined = data;
+    return { data: dataWithType, isLoading, error };
+  }
+
+  return useFetch;
+}
--- packages/react/src/register.ts
+++ packages/react/src/register.ts
@@ -1,30 +1,14 @@
-import { QueryClient, useQuery } from "@tanstack/react-query";
-import { validate } from "./lib/validate";
-import { defaultFetchFn } from "./lib/defaultFetchFn";
-import { ApiModel, BaseApiTypeMap, Options, UseFetchArgs } from "./lib/types";
+import { QueryClient } from "@tanstack/react-query";
+import { ApiModel, BaseApiTypeMap, Options } from "./lib/types";
 import { getQueryClientOptions } from "./tests/getQueryClientOptions";
+import { createUseFetch } from "./createUseFetch";
 
 export function register<ApiTypeMap extends BaseApiTypeMap>(
   apiModel: ApiModel<ApiTypeMap>,
   options?: Options,
 ) {
-  const fetchFn = options?.fetchFn ?? defaultFetchFn;
   const client =
     options?.queryClient ?? new QueryClient(getQueryClientOptions());
-  function useFetch<NAME extends keyof ApiTypeMap>(
-    ...args: UseFetchArgs<ApiTypeMap, NAME>
-  ) {
-    const [stateName, params] = args;
-    type StateType = ApiTypeMap[NAME]["type"];
-    const { url, schema } = apiModel[stateName];
-    const resolvedUrl: string = typeof url === "string" ? url : url(params);
-    const paramKeys = Object.values(params ?? {});
-    const queryKey = [stateName, ...paramKeys];
-    const queryFn = async (): Promise<StateType> =>
-      validate(schema, await fetchFn(resolvedUrl));
-    const { data, isLoading, error } = useQuery({ queryKey, queryFn }, client);
-    const dataWithType: StateType | undefined = data;
-    return { data: dataWithType, isLoading, error };
-  }
+  const useFetch = createUseFetch(apiModel, client, options);
   return { useFetch };
 }
