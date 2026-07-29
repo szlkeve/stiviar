d4cdf811163d3f67b983ac3fb56480b20683c3f8
Keve <sz.l.keve@gmail.com>
2026-07-29 22:00:01 +0200

    height

--- website/src/components/HeroCodeSample.tsx
+++ website/src/components/HeroCodeSample.tsx
@@ -1,10 +1,21 @@
-import {heroCodeSampleHtml} from "@site/src/components/heroCodeSample.generated";
+import { heroCodeSampleHtml } from "@site/src/components/heroCodeSample.generated";
 
 export function HeroCodeSample() {
   return (
-    <div
-      className="hero-code-sample"
-      dangerouslySetInnerHTML={{ __html: heroCodeSampleHtml }}
-    />
+    <div className="hero-code-sample" style={{ height: 400 }}>
+      <style>{`
+        .hero-code-sample { height: 400px; }
+        .hero-code-sample pre.shiki {
+          height: 100%;
+          margin: 0;
+          box-sizing: border-box;
+          overflow: auto;
+        }
+      `}</style>
+      <div
+        style={{ height: "100%" }}
+        dangerouslySetInnerHTML={{ __html: heroCodeSampleHtml }}
+      />
+    </div>
   );
 }
