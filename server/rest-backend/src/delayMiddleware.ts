f5cd265b6dd48dbd2413049bda6c825466ccfe69
Keve <keve@robotx.com>
2026-09-24 23:44:57 +0200

    remove server

--- server/rest-backend/src/delayMiddleware.ts
+++
@@ -1,6 +0,0 @@
-import express from "express";
-
-export const delay =
-  (ms: number) =>
-  (_req: express.Request, _res: express.Response, next: express.NextFunction) =>
-    setTimeout(next, ms);
