14be3eb26cabf4ec88146d09991f464e33b47705
Keve <keve@robotx.com>
2026-09-24 09:23:40 +0200

    remove docusaurus website

--- website/src/examples/jsonplaceholder/Component.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { useFetch } from "@site/src/examples/jsonplaceholder/api";
-
-export function Component() {
-  const { data } = useFetch("users");
-
-  return <div>user: {data?.length}</div>;
-}
--- website/src/examples/raw/Page.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { useFetch } from "./api";
-
-export function Page() {
-  const user = useFetch("user");
-
-  return <div>Hello, {user.data}</div>;
-}
